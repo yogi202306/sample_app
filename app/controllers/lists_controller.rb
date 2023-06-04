2abd0ded055c1fb56e648dd054ad8f4f4a84f931
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-06-04 10:10:16 +0000

    フラッシュメッセージが表示されない

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,7 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
+    flash[:notice] = "投稿が成功しました"
     redirect_to list_path(list.id)
     
   end
